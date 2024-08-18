016e181ec36cabf571c2cb7ab77503e9592544e4
Yazan <yazanalthukair@gmail.com>
2024-08-19 02:45:29 +0300

    minor updates to the code

--- currency converter.java
+++ currency converter.java
@@ -1,14 +1,18 @@
+// currency converter By : 0xYazan 
+
 import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
+
+        System.out.println("This program converts one currency to another. \n\n --------------------------------------\n");
 
         float initialAmount, convertedAmount = 0;
         int currencyFrom, currencyTo;
         String currency1Name, currency2Name = null;
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Choose the currency you want to convert from: ");
+        System.out.println("Select the currency you wish to convert from: ");
         System.out.println("(1) SAR \n(2) EURO \n(3) USD \n(4) JPY");
         currencyFrom = scanner.nextInt();
 
@@ -17,11 +21,11 @@ public class Main {
             case 1:
                 currency1Name = "Saudi Riyals";
 
-                System.out.println("To which currency you want it converted to? : ");
+                System.out.println("Select the currency you want to convert to: ");
                 System.out.println("(1) EURO \n(2) USD \n(3) JPY");
                 currencyTo = scanner.nextInt();
 
-                System.out.println("Enter the amount in SAR: ");
+                System.out.println("Enter the amount in Saudi Riyals:");
                 initialAmount = scanner.nextFloat();
 
                 switch(currencyTo){
@@ -46,17 +50,17 @@ public class Main {
                         break;
                 }
 
-                System.out.println(initialAmount + " " + currency1Name + " equal " + String.format("%.2f",convertedAmount) + currency2Name);
+                System.out.println(initialAmount + " " + currency1Name + " = " + String.format("%.2f",convertedAmount) + " "  + currency2Name);
                 break;
 
             case 2:
                 currency1Name = "Euros";
 
-                System.out.println("To which currency you want it converted to? : ");
+                System.out.println("Select the currency you want to convert to: ");
                 System.out.println("(1) SAR \n(2) USD \n(3) JPY");
                 currencyTo = scanner.nextInt();
 
-                System.out.println("Enter the amount in Euros: ");
+                System.out.println("Enter the amount in Euros:");
                 initialAmount = scanner.nextFloat();
 
                 switch(currencyTo){
@@ -81,18 +85,18 @@ public class Main {
                         break;
                 }
 
-                System.out.println(initialAmount + " " + currency1Name + " equal " + String.format("%.2f",convertedAmount) + currency2Name);
+                System.out.println(initialAmount + " " + currency1Name + " = " + String.format("%.2f",convertedAmount) + " "  + currency2Name);
                 break;
 
 
             case 3:
                 currency1Name = "U.S. Dollars";
 
-                System.out.println("To which currency you want it converted to? : ");
+                System.out.println("Select the currency you want to convert to: ");
                 System.out.println("(1) SAR \n(2) EURO \n(3) JPY");
                 currencyTo = scanner.nextInt();
 
-                System.out.println("Enter the amount in U.S. Dollars: ");
+                System.out.println("Enter the amount in U.S. Dollars:");
                 initialAmount = scanner.nextFloat();
 
                 switch(currencyTo){
@@ -117,17 +121,17 @@ public class Main {
                         break;
                 }
 
-                System.out.println(initialAmount + " " + currency1Name + " equal " + String.format("%.2f",convertedAmount) + currency2Name);
+                System.out.println(initialAmount + " " + currency1Name + " = " + String.format("%.2f",convertedAmount) + " "  + currency2Name);
                 break;
 
             case 4:
                 currency1Name = "Japanese Yen";
 
-                System.out.println("To which currency you want it converted to? : ");
+                System.out.println("Select the currency you want to convert to: ");
                 System.out.println("(1) SAR \n(2) EURO \n(3) USD");
                 currencyTo = scanner.nextInt();
 
-                System.out.println("Enter the amount in Japanese Yen: ");
+                System.out.println("Enter the amount in Japanese Yen:");
                 initialAmount = scanner.nextFloat();
 
                 switch(currencyTo){
@@ -152,7 +156,7 @@ public class Main {
                         break;
                 }
 
-                System.out.println(initialAmount + " " + currency1Name + " equal " + String.format("%.2f",convertedAmount) + currency2Name);
+                System.out.println(initialAmount + " " + currency1Name + " = " + String.format("%.2f",convertedAmount) + " " + currency2Name);
                 break;
 
 
@@ -165,4 +169,3 @@ public class Main {
         scanner.close();
     }
 }
-
